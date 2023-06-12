c516572bc81fcded9d966a0634fb10c546e3a1d9
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-06-12 23:47:02 +0000

    [LeetCode Sync] Runtime - 111 ms (9.38%), Memory - 19.7 MB (68.20%)

---
+++ 0049-group-anagrams/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for word in strs:
+            sorted_word = str(sorted(word))
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+
+        ret_list = []
+        for _,values in anagrams.items():
+            ret_list.append(values)
+        return ret_list
+
