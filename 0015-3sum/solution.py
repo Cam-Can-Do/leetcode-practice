cd63901f5ca085a3f9ae35a683b3ba7f16247531
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-05 01:26:21 +0000

    [LeetCode Sync] Runtime - 1012 ms (21.96%), Memory - 20.5 MB (86.29%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -3,20 +3,26 @@ class Solution:
         res = []
         nums.sort()
 
-        for index, value in enumerate(nums):
-            if index > 0 and value == nums[index-1]:
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            if i > 0 and a == nums[i-1]:
                 continue
             
-            l, r = index + 1, len(nums) - 1
+            l = i + 1
+            r = len(nums) - 1
+
             while l < r:
-                threeSum = nums[l] + nums[r] + value
+                threeSum = a + nums[l] + nums[r]
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
                     l += 1
                 else:
-                    res.append([value, nums[l], nums[r]])
+                    res.append([a, nums[l], nums[r]])
                     l += 1
+                    r -= 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res
+            
