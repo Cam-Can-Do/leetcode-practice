5a70e25d97a7b11dfefd58911417a1253e979371
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-10 20:44:07 +0000

    [LeetCode Sync] Runtime - 62 ms (5.40%), Memory - 16.6 MB (99.95%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -3,13 +3,13 @@ class Solution:
         l = 0
         r = len(nums) - 1
 
+
         while l <= r:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-
             if nums[l] <= nums[m]:
-                if target > nums[m] or target < nums[l]:
+                if nums[l] > target or nums[m] < target:
                     l = m + 1
                 else:
                     r = m - 1
