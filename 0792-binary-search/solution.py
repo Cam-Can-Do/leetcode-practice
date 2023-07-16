8fbaff5fc94b9b2a432cd4ece8c7c1b1f6fbc1bb
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-16 19:30:08 +0000

    [LeetCode Sync] Runtime - 241 ms (6.35%), Memory - 18 MB (94.18%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
         l = 0
         r = len(nums) - 1
         while l <= r:
-            pointer = int((r - l) / 2) + l
-            if nums[pointer] == target:
-                return pointer
-            elif nums[pointer] < target:
-                l = pointer + 1
-            elif nums[pointer] > target:
-                r = pointer - 1
-        return -1 
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
