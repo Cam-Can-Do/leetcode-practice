1a8e2db9627783513afc9190a4de3cf39641e575
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-17 20:48:01 +0000

    [LeetCode Sync] Runtime - 53 ms (9.73%), Memory - 16.8 MB (99.93%)

--- 0074-search-a-2d-matrix/solution.py
+++ 0074-search-a-2d-matrix/solution.py
@@ -1,28 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # first need to determine which row
         top = 0
         bottom = len(matrix) - 1
+        row_len = len(matrix[0])
         row_ptr = 0
         while top <= bottom:
-            row_ptr = (top + bottom) // 2
-            if target > matrix[row_ptr][len(matrix[row_ptr]) - 1]:
+            row_ptr = (top + bottom) // 2 
+            
+            if matrix[row_ptr][0] <= target <= matrix[row_ptr][row_len - 1]:
+                break   # found the correct row to conduct binary search on
+            elif target > matrix[row_ptr][row_len - 1]:
                 top = row_ptr + 1
             elif target < matrix[row_ptr][0]:
                 bottom = row_ptr - 1
-            else:
-                break
-        if not (top <= bottom):
+        
+        if top > bottom:
             return False
-
-        row_ptr = (top + bottom) // 2
-        left = 0
-        right = len(matrix[0]) - 1
-        while left <= right:
-            col_ptr = (left + right) // 2
-            if target > matrix[row_ptr][col_ptr]:
-                left = col_ptr + 1
-            elif target < matrix[row_ptr][col_ptr]:
-                right = col_ptr - 1
-            else:
+        
+        l = 0
+        r = row_len - 1
+        row = matrix[row_ptr]
+        m = 0
+        while l <= r:
+            m = (l + r) // 2
+            if row[m] == target:
                 return True
-        return False
+            elif row[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return False 
+            
+
