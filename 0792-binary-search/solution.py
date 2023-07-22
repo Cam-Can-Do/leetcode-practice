09d17b5a9144c5863500a19c757b1a0cb2d6fe02
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-22 23:21:09 +0000

    [LeetCode Sync] Runtime - 248 ms (6.35%), Memory - 18 MB (70.18%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -3,11 +3,11 @@ class Solution:
         l = 0
         r = len(nums) - 1
         while l <= r:
-            m = (l + r) // 2
+            m = (r + l) // 2
             if nums[m] == target:
                 return m
-            elif nums[m] < target:
-                l = m + 1
-            else:
+            elif nums[m] > target:
                 r = m - 1
+            else:
+                l = m + 1
         return -1
