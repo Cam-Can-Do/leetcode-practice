2b50f235097a69e5dc26d13f671ecfc81301a226
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-25 19:20:24 +0000

    [LeetCode Sync] Runtime - 102 ms (45.93%), Memory - 16.4 MB (96.61%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -7,8 +7,9 @@ class Solution:
         for r in range(len(s)):
             counts[s[r]] = counts.get(s[r], 0) + 1
             maxlen = max(maxlen, counts[s[r]])
-
             if r - l + 1 - maxlen > k:
                 counts[s[l]] -= 1
                 l += 1
-        return r - l + 1
+        return (r - l + 1)
+
+
