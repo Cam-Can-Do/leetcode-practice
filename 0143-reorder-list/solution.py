bdecc23ef5a0289a1d35ad101c7bc2a850905f6e
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-25 19:25:32 +0000

    [LeetCode Sync] Runtime - 103 ms (5.00%), Memory - 26.1 MB (6.32%)

--- 0143-reorder-list/solution.py
+++ 0143-reorder-list/solution.py
@@ -8,28 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        
-        # find middle of list
+
+        # split list
         slow = head
         fast = head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # reverse second half, splitting the list in two!
-        second = slow.next
-        prev = slow.next = None
+
+        second = slow.next        
+        slow.next = prev = None
         while second:
             temp = second.next
             second.next = prev
             prev = second
             second = temp
-            
-        # now prev is the last node
-        # now need to combine lists, starting with the first half
+        
         first = head
         second = prev
-
         while second:
             temp1 = first.next
             temp2 = second.next
@@ -37,7 +34,3 @@ class Solution:
             second.next = temp1
             first = temp1
             second = temp2
-
-
-
-
