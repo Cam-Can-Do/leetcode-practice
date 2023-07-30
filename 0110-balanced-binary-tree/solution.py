120aeda5689dfd2c440983cc010be8fc7f804e8c
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-30 16:47:32 +0000

    [LeetCode Sync] Runtime - 46 ms (51.94%), Memory - 21.4 MB (22.94%)

--- 0110-balanced-binary-tree/solution.py
+++ 0110-balanced-binary-tree/solution.py
@@ -6,16 +6,14 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            left = dfs(root.left)
+            right = dfs(root.right)
+            balanced = abs(right[1]-left[1]) <= 1 and right[0] and left[0]
+            return [balanced, 1 + max(left[1], right[1])]
 
-            def dfs(root):
-                if not root:
-                    return [True, 0]
-
-                left = dfs(root.left)
-                right = dfs(root.right)
-
-                balanced  = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
-                return [balanced, 1 + max(left[1], right[1])]
-            return dfs(root)[0]
-
+        return dfs(root)[0]
 
