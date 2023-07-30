3cf341012baaaa3ae729e8fe6ea6ca8e653e1ddb
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-30 23:33:44 +0000

    [LeetCode Sync] Runtime - 82 ms (5.36%), Memory - 26.1 MB (6.32%)

--- 0143-reorder-list/solution.py
+++ 0143-reorder-list/solution.py
@@ -5,17 +5,14 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        
+        # first need to find the middle of the list
         slow = head
         fast = head.next
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
-        
-        # split and reverse second half
+            fast = fast.next.next 
+
+        # break the link between the halves, setup for reversing second half
         second = slow.next
         slow.next = prev = None
         while second:
@@ -24,13 +21,15 @@ class Solution:
             prev = second
             second = temp
         
+        # now we have prev storing the head of the second list, and head storing the head of the first
+        # need to alternate (starting with first list) and join the links together
         first = head
         second = prev
         while second:
-            temp1 = first.next
-            temp2 = second.next
+            cur1 = first.next
+            cur2 = second.next
             first.next = second
-            second.next = temp1
-            first = temp1
-            second = temp2 
+            second.next = cur1
+            first = cur1
+            second = cur2
         
