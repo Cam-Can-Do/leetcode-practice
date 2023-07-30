490d37c5d95c60c5460a89beed3921ea2a831e59
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-30 23:29:31 +0000

    [LeetCode Sync] Runtime - 36 ms (72.21%), Memory - 16.3 MB (97.42%)

--- 0021-merge-two-sorted-lists/solution.py
+++ 0021-merge-two-sorted-lists/solution.py
@@ -15,6 +15,7 @@ class Solution:
                 cur.next = list2
                 list2 = list2.next
             cur = cur.next
+
         if list1:
             cur.next = list1
         elif list2:
