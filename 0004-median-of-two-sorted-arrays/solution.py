01ef7cce34941cd6920a5c6e26ee733a2c19f11d
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-31 15:58:11 +0000

    [LeetCode Sync] Runtime - 92 ms (8.57%), Memory - 16.7 MB (99.35%)

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,29 +1,34 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
+        total = len(A) + len(B)
         half = total // 2
 
-        if len(B) < len(A):
+        # make A the shorter array
+        if len(A) > len(B):
             A, B = B, A
-        l, r = 0, len(A) - 1
+
+        # for A partitioning
+        l = 0
+        r = len(A) - 1
+
         while True:
-            i = (l + r) // 2 # A
-            j = half - i - 2 # B
-
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i+1] if (i+1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
-
+            i = (l + r) // 2
+            j = half - i - 2
+    
+            aLeft = A[i] if i >= 0 else float("-inf")
+            bLeft = B[j] if j >= 0 else float("-inf")
+            aRight = A[i+1] if i + 1 < len(A) else float("inf")
+            bRight = B[j+1] if j + 1 < len(B) else float("inf")
+    
             # partition is correct
-            if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
-                    return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+            if aLeft <= bRight and bLeft <= aRight:
+                if total % 2 == 0:  # total length is even
+                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
+                else:
+                    return min(aRight, bRight)
+            elif aLeft > bRight:
+                r -= 1
             else:
-                l = i + 1
+                l += 1
+        
