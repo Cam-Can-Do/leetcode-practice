33fc8075c46e466dd64bbb6dfbf1f81ef7729117
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-07-31 16:31:18 +0000

    [LeetCode Sync] Runtime - 699 ms (10.22%), Memory - 78.5 MB (33.94%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,22 +1,33 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key, val
+        self.key, self.val = key,  val
         self.prev = self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
+       self.cap = capacity 
+       self.cache = {}
 
-        self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left
+       self.left = Node(0,0)
+       self.right = Node(0,0)
 
+       self.left.next = self.right
+       self.right.prev = self.left
+
+    # remove from list
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+        
 
+    
+    # insert at right (node is already in self.cache)
     def insert(self, node):
-        prev, nxt = self.right.prev, self.right
+        prev = self.right.prev
+        nxt = self.right
         prev.next = nxt.prev = node
         node.next, node.prev = nxt, prev
 
@@ -26,6 +37,7 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -34,11 +46,11 @@ class LRUCache:
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
+            # remove and delete LRU node (fron linked list and self.cache)
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
 
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
