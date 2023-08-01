a37ce698c7f1b4322d98ee0115ed846f9aecf9da
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-01 20:05:29 +0000

    [LeetCode Sync] Runtime - 90 ms (13.12%), Memory - 16.7 MB (99.35%)

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,34 +1,39 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
+        A = nums1
+        B = nums2
+        if len(A) > len(B):
+            A, B = B, A
         total = len(A) + len(B)
         half = total // 2
 
-        # make A the shorter array
-        if len(A) > len(B):
-            A, B = B, A
-
-        # for A partitioning
         l = 0
         r = len(A) - 1
 
         while True:
             i = (l + r) // 2
             j = half - i - 2
-    
-            aLeft = A[i] if i >= 0 else float("-inf")
-            bLeft = B[j] if j >= 0 else float("-inf")
-            aRight = A[i+1] if i + 1 < len(A) else float("inf")
-            bRight = B[j+1] if j + 1 < len(B) else float("inf")
-    
-            # partition is correct
+
+            aLeft = A[i] if i >= 0 else float("-infinity")
+            aRight = A[i + 1] if i + 1 < len(A) else float("infinity")
+            bLeft = B[j] if j >= 0 else float("-infinity")
+            bRight = B[j + 1] if j + 1 < len(B) else float("infinity")
+
+            # partitions are correct
             if aLeft <= bRight and bLeft <= aRight:
-                if total % 2 == 0:  # total length is even
-                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
-                else:
+                # total size is odd, so just need the middle element
+                if total % 2 != 0:
                     return min(aRight, bRight)
+                else:   # total size is even, so take average of two middle elements
+                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
             elif aLeft > bRight:
-                r -= 1
+                r = i - 1
             else:
-                l += 1
+                l = i + 1
+
+
+
+
+
+
         
