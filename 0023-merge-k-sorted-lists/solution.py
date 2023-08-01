95407b6133272972e7e0c7cc7fefdcd01bcb8efe
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-01 20:47:53 +0000

    [LeetCode Sync] Runtime - 112 ms (20.43%), Memory - 19.9 MB (39.44%)

--- 0023-merge-k-sorted-lists/solution.py
+++ 0023-merge-k-sorted-lists/solution.py
@@ -5,28 +5,31 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge(list1, list2):
-            dummy = ListNode()
-            cur = dummy
-            while list1 and list2:
-                if list1.val <= list2.val:
-                    cur.next = list1
-                    list1 = list1.next
-                else:
-                    cur.next = list2
-                    list2 = list2.next
-                cur = cur.next
-            
-            if list1:
-                cur.next = list1
-            if list2:
-                cur.next = list2
-            return dummy.next
-        if len(lists) == 0:
+        if not lists:
             return None
-        while len(lists) > 1:
-            mergedList = merge(lists.pop(0), lists.pop(0))
-            lists.append(mergedList)
-        return lists[0]
 
+        while len(lists) > 1:
+            l1 = lists.pop(0)
+            l2 = lists.pop(0)
+            lists.append(self.mergeTwoLists(l1, l2))
+        return lists[0]
+            
+
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 and l2:
+            if l1.val > l2.val:
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return dummy.next
 
