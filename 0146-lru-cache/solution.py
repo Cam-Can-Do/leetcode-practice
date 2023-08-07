f4f077b1b32233b5e82651735e5060bd7e753ec4
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-07 03:55:34 +0000

    [LeetCode Sync] Runtime - 722 ms (10.05%), Memory - 78.6 MB (22.67%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,56 +1,50 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key,  val
-        self.prev = self.next = None
+class Node(object):
+    def __init__(self, key = None, val = None, prev = None, next = None):
+        self.key = key
+        self.val = val
+        self.prev = prev 
+        self.next = next
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-       self.cap = capacity 
-       self.cache = {}
+        self.store = {}
+        self.cap = capacity
+        self.lru_ptr = Node()
+        self.mru_ptr = Node()
 
-       self.left = Node(0,0)
-       self.right = Node(0,0)
+        self.lru_ptr.next = self.mru_ptr
+        self.mru_ptr.prev = self.lru_ptr
 
-       self.left.next = self.right
-       self.right.prev = self.left
-
-    # remove from list
-    def remove(self, node):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
-        
-
-    
-    # insert at right (node is already in self.cache)
     def insert(self, node):
-        prev = self.right.prev
-        nxt = self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        node.prev = self.mru_ptr.prev
+        node.next = self.mru_ptr
+        self.mru_ptr.prev.next = node
+        self.mru_ptr.prev = node
+
+    def remove(self, node):
+        temp = node.next
+        node.next.prev = node.prev
+        node.prev.next = temp
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
-        
+        if key in self.store:
+            self.remove(self.store[key])
+            self.insert(self.store[key])
+            return self.store[key].val
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        # if key isn't stored, we need to store and insert it 
+        if key in self.store:
+            self.remove(self.store[key])
+        elif self.cap == len(self.store):
+            del self.store[self.lru_ptr.next.key]
+            self.remove(self.lru_ptr.next)
+        self.store[key] = Node(key, value)
+        self.insert(self.store[key])
 
-        if len(self.cache) > self.cap:
-            # remove and delete LRU node (fron linked list and self.cache)
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
