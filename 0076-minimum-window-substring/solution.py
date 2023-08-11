f33aa4aef150f55a63e91ad2f86d7ac9f15003d6
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-11 16:42:54 +0000

    [LeetCode Sync] Runtime - 102 ms (80.53%), Memory - 17.3 MB (77.54%)

--- 0076-minimum-window-substring/solution.py
+++ 0076-minimum-window-substring/solution.py
@@ -1,39 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if s == "" or t == "":
-            return ""
-
         countsT = {}
-        for letter in t:
-            countsT[letter] = countsT.get(letter, 0) + 1
+        for char in t:
+            countsT[char] = countsT.get(char, 0) + 1
 
+        res = [-1, -1]
+        reslen = float("inf")
+        
         have = 0
         need = len(countsT)
-        
-        res = [-1, -1]
-        reslen = float("infinity")
 
-        l = 0
         window = {}
-        for r in range(len(s)):
-            char = s[r]
-            window[char] = window.get(char, 0) + 1
-            if char in countsT and window[char] == countsT[char]:
-                have += 1
+        l = 0
 
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = window.get(c, 0) + 1
+            if c in countsT and window[c] == countsT[c]:
+                have += 1
+            
             while have == need:
                 if r - l + 1 < reslen:
                     res = [l, r]
                     reslen = r - l + 1
+                
                 window[s[l]] -= 1
                 if s[l] in countsT and window[s[l]] < countsT[s[l]]:
                     have -= 1
+                if window[s[l]] == 0:
+                    del window[s[l]]
                 l += 1
-        if reslen == float("infinity"):
-            return ""
-        else:
-            return s[res[0]:res[1] + 1]
-
-
-
-
+        l = res[0]
+        r = res[1]
+        return "" if reslen == float("inf") else s[l:r+1]
+        
