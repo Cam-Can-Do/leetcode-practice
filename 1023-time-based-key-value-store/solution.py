6b1ee571f5b5320b4c9f085dc2e2ed6cf31f476e
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-11 03:02:03 +0000

    [LeetCode Sync] Runtime - 673 ms (5.98%), Memory - 74.7 MB (24.96%)

--- 1023-time-based-key-value-store/solution.py
+++ 1023-time-based-key-value-store/solution.py
@@ -1,30 +1,32 @@
 class TimeMap:
 
     def __init__(self):
+        # maps key : [value, timestamp]
         self.store = {}
         
+
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.store:
             self.store[key] = []
         self.store[key].append([value, timestamp])
+        
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.store:
             return ""
-        values = self.store[key]
+        
+        selection = self.store[key]
         l = 0
-        r = len(values) - 1
+        r = len(selection) - 1
         res = ""
         while l <= r:
             m = (l + r) // 2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
+            if selection[m][1] <= timestamp:
+                res = selection[m][0]
                 l = m + 1
             else:
                 r = m - 1
         return res
-
-        
 
 
 # Your TimeMap object will be instantiated and called as such:
