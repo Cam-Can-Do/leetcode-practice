d998f830efb88f71e64ff3fcb87061abf86bc7ca
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-11 00:44:57 +0000

    [LeetCode Sync] Runtime - 47 ms (46.99%), Memory - 16.8 MB (99.93%)

--- 0074-search-a-2d-matrix/solution.py
+++ 0074-search-a-2d-matrix/solution.py
@@ -1,35 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # first need to determine which row
         top = 0
         bottom = len(matrix) - 1
         row_len = len(matrix[0])
-        row_ptr = 0
+        row = []
         while top <= bottom:
-            row_ptr = (top + bottom) // 2 
-            
-            if matrix[row_ptr][0] <= target <= matrix[row_ptr][row_len - 1]:
-                break   # found the correct row to conduct binary search on
-            elif target > matrix[row_ptr][row_len - 1]:
-                top = row_ptr + 1
-            elif target < matrix[row_ptr][0]:
-                bottom = row_ptr - 1
+            mid = (top + bottom) // 2
+            if matrix[mid][0] <= target <= matrix[mid][row_len - 1]:
+                row = matrix[mid]
+                break
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
+            elif target > matrix[mid][row_len - 1]:
+                top = mid + 1
         
-        if top > bottom:
+        if not row:
             return False
-        
+
         l = 0
         r = row_len - 1
-        row = matrix[row_ptr]
-        m = 0
         while l <= r:
             m = (l + r) // 2
             if row[m] == target:
                 return True
-            elif row[m] < target:
-                l = m + 1
-            else:
+            elif target < row[m]:
                 r = m - 1
-        return False 
-            
+            else:
+                l = m + 1
+        return False
 
+
+        
+        
