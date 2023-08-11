1ca943cad43a93adf3a4bcd10e526427aad1e14e
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-11 18:15:13 +0000

    [LeetCode Sync] Runtime - 105 ms (5.07%), Memory - 16.6 MB (99.87%)

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,39 +1,38 @@
+# Time: log(min(n, m))
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A = nums1
-        B = nums2
-        if len(A) > len(B):
-            A, B = B, A
-        total = len(A) + len(B)
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
         half = total // 2
 
-        l = 0
-        r = len(A) - 1
+        if len(B) < len(A):
+            A, B = B, A
 
+        l, r = 0, len(A) - 1
         while True:
-            i = (l + r) // 2
-            j = half - i - 2
+            i = (l + r) // 2  # A
+            j = half - i - 2  # B
 
-            aLeft = A[i] if i >= 0 else float("-infinity")
-            aRight = A[i + 1] if i + 1 < len(A) else float("infinity")
-            bLeft = B[j] if j >= 0 else float("-infinity")
-            bRight = B[j + 1] if j + 1 < len(B) else float("infinity")
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
 
-            # partitions are correct
-            if aLeft <= bRight and bLeft <= aRight:
-                # total size is odd, so just need the middle element
-                if total % 2 != 0:
-                    return min(aRight, bRight)
-                else:   # total size is even, so take average of two middle elements
-                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
-            elif aLeft > bRight:
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
                 r = i - 1
             else:
                 l = i + 1
+1
 
-
-
-
-
-
+        
+        
         
