a6eb01c3944919e32c363881334008163e95139d
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-11 00:17:40 +0000

    [LeetCode Sync] Runtime - 203 ms (6.35%), Memory - 18 MB (70.18%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -6,9 +6,9 @@ class Solution:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-            if nums[m] > target:
-                r = m - 1
-            else:
+            elif nums[m] < target:
                 l = m + 1
+            else:
+                r = m - 1
         return -1
-                
+        
