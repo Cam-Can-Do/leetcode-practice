4ea4a67cb0a41a4d7ece956ffc96395d8da31a11
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-12 01:54:54 +0000

    [LeetCode Sync] Runtime - 49 ms (7.58%), Memory - 18.8 MB (43.24%)

--- 0104-maximum-depth-of-binary-tree/solution.py
+++ 0104-maximum-depth-of-binary-tree/solution.py
@@ -6,10 +6,7 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # use a recursive depth first search
-
-        # base case
         if not root:
             return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
