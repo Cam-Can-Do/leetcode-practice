bff8707a58253dbabc997eb75b8f5f17032bb61f
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-12 02:09:42 +0000

    [LeetCode Sync] Runtime - 112 ms (17.71%), Memory - 17.4 MB (95.52%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.py
+++ 0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -3,12 +3,12 @@ class Solution:
         l = 0
         r = len(numbers) - 1
 
-        while l < r:
+        while l <= r:
             tsum = numbers[l] + numbers[r]
             if tsum == target:
                 return [l + 1, r + 1]
-            elif tsum < target:
-                l += 1
-            else:
+            elif tsum > target:
                 r -= 1
-        
+            else:
+                l += 1
+
