49bac64474aa25285b18479958fb4ef48dd6d864
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-12 00:10:32 +0000

    [LeetCode Sync] Runtime - 107 ms (6.51%), Memory - 22 MB (25.48%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,19 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        freqs = {}
+        for num in nums:
+            freqs[num] = freqs.get(num, 0) + 1
+        
+        # next, need to group/sort numbers by frequency
 
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
+        groups = [[] for i in range(len(nums) + 1)]
+
+        for num, freq in freqs.items():
+            groups[freq].append(num)
 
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
+
+        for i in range(len(groups) - 1, -1, -1):
+            # i is index in groups... groups[i] is an array
+            vals = groups[i]
+            for val in vals:
+                if k == 0:
                     return res
-
-        # O(n)
-
+                res.append(val)
+                k -= 1
+        return res
