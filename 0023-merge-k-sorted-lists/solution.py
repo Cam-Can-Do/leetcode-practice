190753bb93dfe8d8252ec5af2f3220b4b660bea4
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-12 00:16:25 +0000

    [LeetCode Sync] Runtime - 95 ms (24.26%), Memory - 19.9 MB (16.88%)

--- 0023-merge-k-sorted-lists/solution.py
+++ 0023-merge-k-sorted-lists/solution.py
@@ -5,31 +5,35 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # merge lists two at a time
+
+
+        def mergeTwo(l1, l2):
+            dummy = ListNode()
+            cur = dummy
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    cur.next = l1
+                    l1 = l1.next
+                else:
+                    cur.next = l2
+                    l2 = l2.next
+                cur = cur.next
+
+            if l1:
+                cur.next = l1
+            elif l2:
+                cur.next = l2
+            return dummy.next
+
+
         if not lists:
             return None
 
         while len(lists) > 1:
             l1 = lists.pop(0)
             l2 = lists.pop(0)
-            lists.append(self.mergeTwoLists(l1, l2))
+            merged = mergeTwo(l1, l2)
+            lists.append(merged)
+        
         return lists[0]
-            
-
-    def mergeTwoLists(self, l1, l2):
-        dummy = ListNode(0)
-        cur = dummy
-        while l1 and l2:
-            if l1.val > l2.val:
-                cur.next = l2
-                l2 = l2.next
-            else:
-                cur.next = l1
-                l1 = l1.next
-            cur = cur.next
-
-        if l1:
-            cur.next = l1
-        if l2:
-            cur.next = l2
-        return dummy.next
-
