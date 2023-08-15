f697891f893fb1a5df717548371ba0781c7561d4
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-15 03:15:39 +0000

    [LeetCode Sync] Runtime - 84 ms (39.09%), Memory - 16.5 MB (99.95%)

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,38 +1,39 @@
-# Time: log(min(n, m))
-
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
 
-        if len(B) < len(A):
+        if total == 0:
+            return 0
+
+        A = nums1
+        B = nums2
+        if len(A) > len(B):
             A, B = B, A
 
-        l, r = 0, len(A) - 1
+        l = 0
+        r = len(A) - 1
         while True:
-            i = (l + r) // 2  # A
-            j = half - i - 2  # B
-
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
-
+            aMiddle = (r + l) // 2
+            bMiddle = half - aMiddle - 2
+    
+            aLeft = A[aMiddle] if aMiddle >= 0 else float("-inf")
+            aRight = A[aMiddle + 1] if aMiddle + 1 < len(A) else float("inf")
+            bLeft = B[bMiddle] if bMiddle >= 0 else float("-inf")
+            bRight = B[bMiddle + 1] if bMiddle + 1 < len(B) else float("inf")
+    
             # partition is correct
-            if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
-                    return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+            if aLeft <= bRight and bLeft <= aRight: 
+                # total length is even
+                if total % 2 == 0:
+                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
+                else:
+                    return min(aRight, bRight)
+            # incorrect partition
             else:
-                l = i + 1
-1
+                if aLeft > bRight:
+                    r = aMiddle - 1
+                else:
+                    l = aMiddle + 1
 
-        
-        
-        
+
