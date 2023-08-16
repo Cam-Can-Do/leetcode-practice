2c2ea5796c101597c363c58f73eef5be7a250aae
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-16 02:44:30 +0000

    [LeetCode Sync] Runtime - 42 ms (67.18%), Memory - 16.6 MB (99.62%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -7,18 +7,17 @@ class Solution:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-            # right section is not sorted, rotated
-            if nums[m] > nums[r]:
-                if nums[l] > target or target > nums[m]:
+            # right portion is rotated
+            elif nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l]:
                     l = m + 1
                 else:
                     r = m - 1
-
-            # right is in sorted order
             else:
-                if nums[m] > target or nums[r] < target:
+                if target < nums[m] or target > nums[r]:
                     r = m - 1
                 else:
                     l = m + 1
-            
         return -1
+
+        
