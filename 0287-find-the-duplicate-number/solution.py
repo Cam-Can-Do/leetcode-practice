4526b0ce1d3a7620785c3b41ce90ad6ed16ab541
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-17 21:51:58 +0000

    [LeetCode Sync] Runtime - 546 ms (7.58%), Memory - 30.4 MB (98.22%)

--- 0287-find-the-duplicate-number/solution.py
+++ 0287-find-the-duplicate-number/solution.py
@@ -5,14 +5,14 @@ class Solution:
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
+
             if slow == fast:
                 break
-            
         slow2 = 0
         while True:
-            slow2 = nums[slow2]
             slow = nums[slow]
+            slow2 = nums[slow2]
+
             if slow == slow2:
                 return slow2
         
-
