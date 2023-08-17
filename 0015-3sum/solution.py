8a98da4930d577882c24fe773631e5769755fc72
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-17 21:20:31 +0000

    [LeetCode Sync] Runtime - 813 ms (31.68%), Memory - 20.5 MB (86.29%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -1,29 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         res = []
-        nums = sorted(nums)
-
-        for i, val in enumerate(nums):
-            if val > 0:
-                break
-
-            if i > 0 and val == nums[i - 1]:
+        i = 0
+        for i, a, in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
+            if a > 0:
+                break
+            
             l = i + 1
             r = len(nums) - 1
             while l < r:
-                threeSum = nums[i] + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
+                threeSum = a + nums[l] + nums[r]
+                if threeSum == 0:
+                    res.append([a, nums[l], nums[r]])
                     l += 1
                     r -= 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    r -= 1
         return res
-                    
+
         
