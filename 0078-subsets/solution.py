a6b9423a41d8f686b368ba95cf0497d46def0ff9
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-22 15:14:25 +0000

    [LeetCode Sync] Runtime - 51 ms (5.51%), Memory - 16.5 MB (99.62%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
         subset = []
-
-        i = 0
+        res = []
+        
         def dfs(i):
             if i >= len(nums):
                 res.append(subset.copy())
-                return None
-
+                return
 
             subset.append(nums[i])
             dfs(i+1)
-        
             subset.pop()
             dfs(i+1)
         dfs(0)
