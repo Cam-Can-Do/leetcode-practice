f0684546f61990988a3192d090734481eee4df77
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-22 13:56:45 +0000

    [LeetCode Sync] Runtime - 118 ms (50.00%), Memory - 18.2 MB (98.54%)

--- 0042-trapping-rain-water/solution.py
+++ 0042-trapping-rain-water/solution.py
@@ -2,19 +2,22 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         l = 0
         r = len(height) - 1
-        
-        leftMax = height[l]
-        rightMax = height[r]
+
+        lMax = height[l]
+        rMax = height[r]
 
         total = 0
-        while l < r:
-            if leftMax < rightMax:
+        while l <= r:
+            m = (l + r) // 2
+            if lMax <= rMax:
+                lMax = max(lMax, height[l])
+                total += lMax - height[l]
                 l += 1
-                leftMax = max(leftMax, height[l])
-                total += leftMax - height[l]
             else:
+                rMax = max(rMax, height[r])
+                total += rMax - height[r]
                 r -= 1
-                rightMax = max(rightMax, height[r])
-                total += rightMax - height[r]
         return total
 
+
+        
