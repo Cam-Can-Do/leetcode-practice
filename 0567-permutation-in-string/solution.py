0e88e636b7bf5d7b8c447b52c1098a8067fce3d3
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-25 14:18:35 +0000

    [LeetCode Sync] Runtime - 60 ms (61.69%), Memory - 16.3 MB (99.87%)

--- 0567-permutation-in-string/solution.py
+++ 0567-permutation-in-string/solution.py
@@ -1,22 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_chars = {}
-        for char in s1:
-            s1_chars[char] = s1_chars.get(char, 0) + 1
+        s1_counts = {}
+        for letter in s1:
+            s1_counts[letter] = s1_counts.get(letter, 0) + 1
         
         l = 0
         window = {}
+
         for r in range(len(s2)):
             window[s2[r]] = window.get(s2[r], 0) + 1
 
-            if window == s1_chars:
+            if window == s1_counts:
                 return True
-
-            if r - l + 1 >= len(s1):
+            
+            while r - l + 1 >= len(s1):
                 window[s2[l]] -= 1
-                if window[s2[l]] == 0:
+                if window[s2[l]] <= 0:
                     del window[s2[l]]
                 l += 1
 
         return False
+
         
