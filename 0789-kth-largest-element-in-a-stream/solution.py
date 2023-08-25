58c206fea3252f73baa80ee681b3f72d4ca1aecd
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-25 14:28:06 +0000

    [LeetCode Sync] Runtime - 95 ms (38.50%), Memory - 20.5 MB (94.87%)

--- 0789-kth-largest-element-in-a-stream/solution.py
+++ 0789-kth-largest-element-in-a-stream/solution.py
@@ -4,14 +4,15 @@ class KthLargest:
         self.k = k
         self.minHeap = nums
         heapq.heapify(self.minHeap)
-        while len(self.minHeap) > self.k:
+        while len(self.minHeap) > k:
             heapq.heappop(self.minHeap)
-        
+
     def add(self, val: int) -> int:
         heapq.heappush(self.minHeap, val)
         while len(self.minHeap) > self.k:
             heapq.heappop(self.minHeap)
         return self.minHeap[0]
+
         
 
 
