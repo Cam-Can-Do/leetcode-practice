9358ccaced144af059c21720a294a745ce054de6
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-30 15:14:16 +0000

    [LeetCode Sync] Runtime - 121 ms (55.23%), Memory - 17.1 MB (98.52%)

--- 0076-minimum-window-substring/solution.py
+++ 0076-minimum-window-substring/solution.py
@@ -1,36 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        countsT = {}
-        for char in t:
-            countsT[char] = countsT.get(char, 0) + 1
+        # need to count the frequencies of letters in t
+        tCounts = {}
+        for letter in t:
+            tCounts[letter] = tCounts.get(letter, 0) + 1
+        
+        have = 0
+        need = len(tCounts)
 
         res = [-1, -1]
         reslen = float("inf")
-        
-        have = 0
-        need = len(countsT)
 
         window = {}
         l = 0
 
         for r in range(len(s)):
-            c = s[r]
-            window[c] = window.get(c, 0) + 1
-            if c in countsT and window[c] == countsT[c]:
+            window[s[r]] = window.get(s[r], 0) + 1
+            if s[r] in tCounts and window[s[r]] == tCounts[s[r]]:
                 have += 1
             
-            while have == need:
+            while have >= need:
                 if r - l + 1 < reslen:
                     res = [l, r]
                     reslen = r - l + 1
-                
+                    
                 window[s[l]] -= 1
-                if s[l] in countsT and window[s[l]] < countsT[s[l]]:
+                if s[l] in tCounts and window[s[l]] < tCounts[s[l]]:
                     have -= 1
                 if window[s[l]] == 0:
                     del window[s[l]]
                 l += 1
-        l = res[0]
-        r = res[1]
-        return "" if reslen == float("inf") else s[l:r+1]
+        if reslen != float("inf"):
+            return s[res[0]:res[1]+1]
+        else:
+            return ""
+
+
+
+        
         
