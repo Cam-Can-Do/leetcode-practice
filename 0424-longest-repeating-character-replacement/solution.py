af358ee1c0aaed89055e83165ef61193800c5315
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-08-31 14:28:34 +0000

    [LeetCode Sync] Runtime - 85 ms (67.85%), Memory - 16.5 MB (96.61%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,19 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        counts = {}
-        highestCount = 0
         l = 0
+
+        counts = {}
+        longest = 0
         for r in range(len(s)):
             counts[s[r]] = counts.get(s[r], 0) + 1
-            highestCount = max(counts[s[r]], highestCount)
+            longest = max(longest, counts[s[r]])
 
-            if r - l + 1 - highestCount > k:
+            if r - l + 1 - longest > k:
                 counts[s[l]] -= 1
                 if counts[s[l]] == 0:
                     del counts[s[l]]
                 l += 1
-            
         return r - l + 1
-
 
         
