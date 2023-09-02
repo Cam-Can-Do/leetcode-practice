7819c6bc8396499fd4b418abbbe910860fca6f2f
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2023-09-02 13:28:25 +0000

    [LeetCode Sync] Runtime - 352 ms (6.92%), Memory - 17.8 MB (99.79%)

--- 0907-koko-eating-bananas/solution.py
+++ 0907-koko-eating-bananas/solution.py
@@ -1,21 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1
-        r = max(piles)
-        res = r
+        # binary search with eating speeds for the lowest valid eating speed
+        # that allows koko to eat all bananas before guards return
 
+        l = 1 # starts at 1 because 0 bananas will get nothing done...
+        r = max(piles)
+                            
+        res = r
         while l <= r:
             m = (l + r) // 2
-            timeElapsed = 0
+            time = 0
             for pile in piles:
-                timeElapsed += math.ceil(pile / m)
-            # valid speed
-            if timeElapsed <= h:
+                time += ceil(pile / m)
+            if time <= h:
+                res = m
                 r = m - 1
-                res = min(res, m)
             else:
                 l = m + 1
-
         return res
             
-        
