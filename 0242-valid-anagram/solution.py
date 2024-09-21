c6d6d8ae1cf529b0b5d66b41c02034c2f5f753ae
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2024-09-21 17:06:08 +0000

    [LeetCode Sync] Runtime - 58 ms (14.78%), Memory - 17.5 MB (5.83%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,10 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        countsS = {}
-        for char in s:
-            countsS[char] = countsS.get(char, 0) + 1
-        countsT = {}
-        for char in t:
-            countsT[char] = countsT.get(char, 0) + 1
-        return countsT == countsS
-        
+        return sorted(s) == sorted(t)
+            
