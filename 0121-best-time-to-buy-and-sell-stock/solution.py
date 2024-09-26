70ac25b832426674a0b86c07ef68d04c516672dd
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2024-09-26 05:08:57 +0000

    [LeetCode Sync] Runtime - 763 ms (37.64%), Memory - 27.4 MB (42.94%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
         lowest = prices[0]
-        profit = 0 # default return
+        profit = 0
 
         for price in prices:
             lowest = min(lowest, price)
