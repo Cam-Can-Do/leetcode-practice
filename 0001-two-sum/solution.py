64e046dcb05b217ac40c0ea10b582cca7e349875
Cameron Candau <121522527+Cam-Can-Do@users.noreply.github.com>
2024-09-28 06:44:04 +0000

    [LeetCode Sync] Runtime - 55 ms (80.48%), Memory - 17.8 MB (33.42%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        val_to_index = {}
+        contained = {}
         for i, num in enumerate(nums):
             diff = target - num
-            if diff in val_to_index:
-                return [i, val_to_index[diff]]
-            val_to_index[num] = i
+            if diff in contained:
+                return [i, contained[diff]]
+            contained[num] = i
+        
